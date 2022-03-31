eac6202ac386aef1074df99038b5d7b82e0ed6e1
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-03-31 18:42:49 +0530

    Time: 2 ms (100.00%), Space: 42.3 MB (95.85%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -2,47 +2,38 @@ class Solution {
     public int romanToInt(String s) {
         int n=s.length();
         int sum=0,p=0;
-        char c=' ';
         while(n-->0)
         {
-            int res=0;
-            c=s.charAt(n);
-            p=evaluate(c,p);
+            int temp;
+            temp=evaluate(s.charAt(n));
+            p=temp<p?(-1)*temp:temp;
             sum=sum+p;
         }
         //System.out.print("_");
         return sum;
     }
-    public int evaluate(char c,int p)
+    public int evaluate(char c)
     {
         //System.out.print("_");
-        int res=0;
         switch(c)
         {
             case 'I':
-                res=1;
-                break;
+                return 1;
             case 'V':
-                res=5;
-                break;
+                return 5;
             case 'X': 
-                res=10;
-                break;
+                return 10;
             case 'L': 
-                res=50;
-                break;
+                return 50;
             case 'C': 
-                res=100;
-                break;
+               return 100;
             case 'D': 
-                res=500;
-                break;
+                return 500;
             case 'M': 
-                res=1000;
-                break;
+                return 1000;
         }
-        if(res<p) return (-1)*res;
-        return res;
+        //if(res<p) return (-1)*res;
+        return 0;
     }
     
     
