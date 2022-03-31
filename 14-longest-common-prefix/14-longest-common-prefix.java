a83aa110a04b7346058253bd5b1ef6d568f168d7
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-03-31 23:06:33 +0530

    Time: 1 ms (81.87%), Space: 40 MB (96.21%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -8,12 +8,16 @@ class Solution {
             int j=1;
             for( ;j<strs.length;j++)
             {
-                if(strs[j].length()<len) len=strs[j].length();
+                //if(strs[j].length()<len) len=strs[j].length();
                // System.out.println(j+"_"+i+"_"+strs[j]+"_"+len);
-                if(i>=len) flag=true;
-                else if(strs[j].charAt(i)!=strs[j-1].charAt(i)) flag=true;
+                if(j==1)
+                    if(strs[j].length()<len)
+                         len=strs[j].length();
+                if(strs[j].length()-1<i) return res;;
+                //if(i>=len) flag=true;
+                //else
+                if(strs[j].charAt(i)!=strs[j-1].charAt(i)) return res;
             }
-            if(flag) break;
             res=res+String.valueOf(strs[j-1].charAt(i));
         }
         return res;
