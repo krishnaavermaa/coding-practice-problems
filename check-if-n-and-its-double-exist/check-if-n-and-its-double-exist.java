acaf84d9de0ce05b3e556a7ca0065bf2d762507b
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-01 13:29:08 +0530

    Time: 3 ms, Memory: 43.6 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -2,9 +2,6 @@ import java.util.*;
 class Solution {
     public boolean checkIfExist(int[] arr) {
         qsort(arr,0,0,arr.length-1);
-        // for(int i=0;i<arr.length;i++)
-        //     System.out.print(arr[i]+" ");
-        // System.out.println();
         for(int i=0;i<arr.length;i++)
         {
             int j=arr[i]*2;
@@ -19,13 +16,8 @@ class Solution {
     }
     private void qsort(int arr[],int p,int si,int ei)
     {
-        // System.out.println("**FOR:");
-        // for(int i=0;i<arr.length;i++)
-        //     System.out.print(arr[i]+" ");
-        // System.out.println("\n**");
         if(ei<si) return;
         int s=si,e=ei;
-        // System.out.println("INSIDE START");
         while(e>s)
         {
             while(arr[s]<=arr[p])
@@ -53,19 +45,11 @@ class Solution {
             int tmp=arr[s];
             arr[s]=arr[e];
             arr[e]=tmp;
-            // for(int i=0;i<arr.length;i++)
-            //     System.out.print(arr[i]+" ");
-            // System.out.println();
             
         }
-        // System.out.println("INSIDE END");
-        // System.out.println("for "+arr[p]+" at index "+p+" s="+s+" e="+e);
         int tmp=arr[s];
         arr[s]=arr[p];
         arr[p]=tmp;
-        // for(int i=0;i<arr.length;i++)
-        //     System.out.print(arr[i]+" ");
-        // System.out.println();
         qsort(arr,p,p,s-1);
         qsort(arr,s+1,s+1,ei);
     }
