0938e0d037d625ef34d99a6141b668b466159610
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-02 13:05:33 +0530

    Time: 21 ms (65.49%), Space: 87 MB (66.12%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -6,26 +6,83 @@ class Solution {
         {
             arr[i]=capacity[i]-rocks[i];
         }
-        Queue<Integer> pq=new PriorityQueue<>();
-        for(int a:arr)
-        {
-            pq.add(a);
-        }
-        while(pq.peek()==0)
-        {
+        
+        //qsort(arr,0,0,len-1);
+        Arrays.sort(arr);
+        int i=0;
+        while(arr[i++]==0){
             c++;
-            pq.poll();
+            if(i>=len) return c;
         }
-        while(additionalRocks>0 && !pq.isEmpty())
+        for(i-- ;i<arr.length;i++)
         {
-            
-            if(additionalRocks>= pq.peek())
-            {
+            if(arr[i]<=additionalRocks){
                 c++;
-                additionalRocks-=pq.poll();
+                additionalRocks-=arr[i];
             }
-            else break;
+            else return c;
         }
+        
+//         USING PRIORITY QUEUE:O(nlogn) 
+//         Queue<Integer> pq=new PriorityQueue<>();
+//         for(int a:arr)
+//         {
+//             pq.add(a);
+//         }
+//         while(pq.peek()==0)
+//         {
+//             c++;
+//             pq.poll();
+//         }
+//         while(additionalRocks>0 && !pq.isEmpty())
+//         {
+            
+//             if(additionalRocks>= pq.peek())
+//             {
+//                 c++;
+//                 additionalRocks-=pq.poll();
+//             }
+//             else break;
+//         }
         return c;
     }
+    
+    // public void qsort(int []arr,int p,int si,int ei)
+    // {
+    //     int s=si,e=ei;
+    //     if(ei<=si) return;
+    //     while(s<e)
+    //     {
+    //         while(arr[s]<=arr[p])
+    //         {
+    //             s++;
+    //             if(s>ei){
+    //                 s--;
+    //                 break;
+    //             }
+    //         }
+    //         while(arr[e]>arr[p])
+    //         {
+    //             e--;
+    //             if(e>si)
+    //             {
+    //                 e++;
+    //                 break;
+    //             }
+    //         }
+    //         if(s>e){
+    //             s--;
+    //             e++;
+    //             break;
+    //         }
+    //         int tmp=arr[s];
+    //         arr[s]=arr[e];
+    //         arr[e]=tmp;
+    //     }
+    //     int tmp=arr[s];
+    //     arr[s]=arr[p];
+    //     arr[p]=tmp;
+    //     qsort(arr,p,si,s-1);
+    //     qsort(arr,s+1,s+1,ei);
+    // }
 }
