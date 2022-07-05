80abeffbb2c1c06094cbe7c3244ed96786e7bbe3
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-06 00:18:26 +0530

    Time: 5 ms (45.76%), Space: 44.8 MB (83.23%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -24,71 +24,71 @@ class Solution {
         }
         return min;
     }
-    public void updateCandy(boolean check[],int ratings[], int []candy,int i)
-    {
-        // System.out.println("START i="+i+" "+check[i]);
-        if(check[i]==true) return;
-        int pr=i>0?ratings[i-1]:-1;
-        int nr=i<ratings.length-1?ratings[i+1]:-1;
-        int cr=ratings[i];
-        if(pr==-1)
-        {
-            // System.out.println(i+" pr=-1");
-            if(nr>=cr){
-                candy[i]=1;
-                // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-            }
-            else {
-                updateCandy(check,ratings,candy,i+1);
-                candy[i]=candy[i+1]+1;
-                // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-            }
-        }
-        else if(nr==-1)
-        {
-            // System.out.println(i+" nr=-1");
-            if(pr>=cr){
-                candy[i]=1;
-                // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-            }
-            else {
-                updateCandy(check,ratings,candy,i-1);
-                candy[i]=candy[i-1]+1;
-                // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-            }
-        }
-        else if(cr<=pr && cr<=nr)
-        {
-            // System.out.println(i+" cr<=pr && cr<=nr");
-            candy[i]=1;
-            // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-        }
-        else if(cr>pr && cr<=nr)
-        {
-            // System.out.println(i+" cr>pr && cr<=nr");
-            updateCandy(check,ratings,candy,i-1);
-            candy[i]=candy[i-1]+1;
-            // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-        }
-        else if(cr>nr && cr<=pr)
-        {
-            // System.out.println(i+" cr>nr && cr<=pr");
-            updateCandy(check,ratings,candy,i+1);
-            candy[i]=candy[i+1]+1;
-            // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-        }
-        else if(cr>pr && cr>nr)
-        {
-            // System.out.println(i+" cr>pr && cr>nr");
-            updateCandy(check,ratings,candy,i-1);
-            updateCandy(check,ratings,candy,i+1);
-            candy[i]=Math.max(candy[i-1],candy[i+1])+1;
-            // System.out.println("*\t"+i+" candy="+candy[i]);//***********
-        }
-        check[i]=true;
-        // System.out.println("END i="+i+" "+check[i]);
-    }
-    /* THE ABOVE FUNCTION CAN BE REDUCED AS (recheck needed a lil)
+    // public void updateCandy(boolean check[],int ratings[], int []candy,int i)
+    // {
+    //     // System.out.println("START i="+i+" "+check[i]);
+    //     if(check[i]==true) return;
+    //     int pr=i>0?ratings[i-1]:-1;
+    //     int nr=i<ratings.length-1?ratings[i+1]:-1;
+    //     int cr=ratings[i];
+    //     if(pr==-1)
+    //     {
+    //         // System.out.println(i+" pr=-1");
+    //         if(nr>=cr){
+    //             candy[i]=1;
+    //             // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //         }
+    //         else {
+    //             updateCandy(check,ratings,candy,i+1);
+    //             candy[i]=candy[i+1]+1;
+    //             // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //         }
+    //     }
+    //     else if(nr==-1)
+    //     {
+    //         // System.out.println(i+" nr=-1");
+    //         if(pr>=cr){
+    //             candy[i]=1;
+    //             // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //         }
+    //         else {
+    //             updateCandy(check,ratings,candy,i-1);
+    //             candy[i]=candy[i-1]+1;
+    //             // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //         }
+    //     }
+    //     else if(cr<=pr && cr<=nr)
+    //     {
+    //         // System.out.println(i+" cr<=pr && cr<=nr");
+    //         candy[i]=1;
+    //         // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //     }
+    //     else if(cr>pr && cr<=nr)
+    //     {
+    //         // System.out.println(i+" cr>pr && cr<=nr");
+    //         updateCandy(check,ratings,candy,i-1);
+    //         candy[i]=candy[i-1]+1;
+    //         // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //     }
+    //     else if(cr>nr && cr<=pr)
+    //     {
+    //         // System.out.println(i+" cr>nr && cr<=pr");
+    //         updateCandy(check,ratings,candy,i+1);
+    //         candy[i]=candy[i+1]+1;
+    //         // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //     }
+    //     else if(cr>pr && cr>nr)
+    //     {
+    //         // System.out.println(i+" cr>pr && cr>nr");
+    //         updateCandy(check,ratings,candy,i-1);
+    //         updateCandy(check,ratings,candy,i+1);
+    //         candy[i]=Math.max(candy[i-1],candy[i+1])+1;
+    //         // System.out.println("*\t"+i+" candy="+candy[i]);//***********
+    //     }
+    //     check[i]=true;
+    //     // System.out.println("END i="+i+" "+check[i]);
+    // }
+    // /* THE ABOVE FUNCTION CAN BE REDUCED AS (recheck needed a lil)
     public void updateCandy(boolean check[],int ratings[], int []candy,int i)
     {
         if(check[i]==true) return;
@@ -100,11 +100,16 @@ class Solution {
             updateCandy(check,ratings,candy,i+1);
             candy[i]=candy[i+1]+1;
         }
-        else if(cr>pr && (nr==-1 || cr<=pr)){
+        else if(cr>pr && (nr==-1 || cr<=nr)){
             updateCandy(check,ratings,candy,i-1);
             candy[i]=candy[i-1]+1;
         }
+        else if(cr>pr && cr>nr){
+            updateCandy(check,ratings,candy,i-1);
+            updateCandy(check,ratings,candy,i+1);
+            candy[i]=Math.max(candy[i-1],candy[i+1])+1;
+        }
         check[i]=true;
     }
-*/
+// */
 }
