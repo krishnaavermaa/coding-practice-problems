c479bc8129bb3d946d5244fa71bfd3e6d1066c60
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-06 16:39:48 +0530

    Time: 0 ms, Memory: 42.5 MB - LeetHub

--- linked-list-cycle-ii/linked-list-cycle-ii.java
+++ linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -18,36 +18,13 @@ public class Solution {
             q=q.next.next;
             if(p==q) break;
         }
-        System.out.print("*");
         if(p==q && q!=null && q.next!=null){
-            System.out.print("_-");
             while(head!=p && p.next!=null){
-                System.out.print("\n"+p.val+" "+head.val+"\n");
                 p=p.next;
                 head=head.next;
             }
-            System.out.print("%");
-
             return head;
         }
         return null;
     }
 }
-
-// public class Solution {
-//     public ListNode detectCycle(ListNode head) {
-//         ListNode slow = head, fast = head;
-//         while (fast != null && fast.next != null) {
-//             slow = slow.next;
-//             fast = fast.next.next;
-//             if (slow == fast) break;
-//         }
-//         if (fast == null || fast.next == null) return null;
-//         while (head != slow) {
-//             System.out.print("\n"+slow.val+" "+head.val+"\n");
-//             head = head.next;
-//             slow = slow.next;
-//         }
-//         return head;
-//     }
-// }
