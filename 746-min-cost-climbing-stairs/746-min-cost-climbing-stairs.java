8a13822055b41e6e74ee9bb9cb11539f56ba092c
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-10 22:34:11 +0530

    Time: 1 ms (89.45%), Space: 43.7 MB (42.84%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,30 +1,48 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int mem[]=new int[cost.length];
-        int zeroStarting=0;
-        int oneStarting=0;
-        Arrays.fill(mem,-1);
+//     public int minCostClimbingStairs(int[] cost) {
+//         int mem[]=new int[cost.length];
+//         int zeroStarting=0;
+//         int oneStarting=0;
+//         Arrays.fill(mem,-1);
         
-        if(mem[0]==-1){
-            zeroStarting=cost(cost,0,mem);
-        }
-        else zeroStarting=mem[0];
-        if(mem[1]==-1){
-            oneStarting=cost(cost,1,mem);
-        }
-        else oneStarting=mem[1];
-        return zeroStarting<=oneStarting?zeroStarting:oneStarting;
-    }
-    public int cost(int []cost,int index,int mem[])
+//         if(mem[0]==-1){
+//             zeroStarting=cost(cost,0,mem);
+//         }
+//         else zeroStarting=mem[0];
+//         if(mem[1]==-1){
+//             oneStarting=cost(cost,1,mem);
+//         }
+//         else oneStarting=mem[1];
+//         return zeroStarting<=oneStarting?zeroStarting:oneStarting;
+//     }
+//     public int cost(int []cost,int index,int mem[])
+//     {
+//         if(index>=cost.length){
+//             return 0;
+//         }
+//         if(mem[index]==-1){
+//             int leftc=cost[index]+cost(cost,index+1,mem);
+//             int rightc=cost[index]+cost(cost,index+2,mem);
+//             mem[index]=rightc<=leftc?rightc:leftc;
+//         }
+//         return mem[index];
+//     }
+    public int minCostClimbingStairs(int[] cost) 
     {
-        if(index>=cost.length){
-            return 0;
+        int res=0;
+        int len=cost.length;
+        //base case
+        //using 2 pointer for tabulation/bottom up approach
+        int last=cost[1];
+        int last2last=cost[0];
+        
+        for(int i=2;i<len;i++)
+        {
+            int tmp=last;
+            last=cost[i]+(last2last<=last?last2last:last);
+            last2last=tmp;
         }
-        if(mem[index]==-1){
-            int leftc=cost[index]+cost(cost,index+1,mem);
-            int rightc=cost[index]+cost(cost,index+2,mem);
-            mem[index]=rightc<=leftc?rightc:leftc;
-        }
-        return mem[index];
+        return last2last<=last?last2last:last;
     }
+
 }
