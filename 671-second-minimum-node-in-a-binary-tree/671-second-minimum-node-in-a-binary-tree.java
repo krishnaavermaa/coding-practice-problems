fbe845a9b3ccc4d615b53b724f4f65eb0216d32a
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-28 00:15:06 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (40.29%) - LeetHub

--- 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.java
+++ 671-second-minimum-node-in-a-binary-tree/671-second-minimum-node-in-a-binary-tree.java
@@ -14,19 +14,21 @@
  * }
  */
 class Solution {
+    int rootval;
     public int findSecondMinimumValue(TreeNode root) {
-        int res=func(root.val,root);
-        return res!=root.val?res:-1;
+        rootval=root.val;
+        int res=func(root);
+        return res!=rootval?res:-1;
     }
-    public int func(int rootval, TreeNode node)
+    public int func(TreeNode node)
     {
         if(node.left==null && node.right==null) return node.val;
         int l=-1,r=-1;
         if(node.left.val!=node.right.val) {
             l=node.left.val;
             r=node.right.val;
-            if(l<r && l==rootval) l=func(rootval,node.left);
-            else if(r<l && r==rootval) r=func(rootval,node.right);
+            if(l<r && l==rootval) l=func(node.left);
+            else if(r<l && r==rootval) r=func(node.right);
             if(l!=rootval && r!=rootval)
             {
                 if(l==-1||r==-1) return l!=-1?l:r;
@@ -36,8 +38,8 @@ class Solution {
         }
         else
         {
-            l=func(rootval,node.left);
-            r=func(rootval,node.right);
+            l=func(node.left);
+            r=func(node.right);
             if(l==r) return -1;
             if(l!=rootval && r!=rootval)
             {
