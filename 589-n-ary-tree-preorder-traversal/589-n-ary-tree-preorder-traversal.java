36dbeb9123dbf10b78752fb13044b6addcc9b911
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-07-28 10:02:15 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (98.14%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> list=new LinkedList<>();
+        if(root==null) return list;
+        recurr(root,list);
+        return list;
+    }
+    public void recurr(Node node, List<Integer> list)
+    {
+        list.add(node.val);
+        if(!node.children.isEmpty())
+            for(int i=0;i<node.children.size();i++)
+                recurr(node.children.get(i),list);
+    }
+}
