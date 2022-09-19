c41b91174efc174309bdeb9c703a4a035bcd83a4
Krishna Verma <56075658+krishnaavermaa@users.noreply.github.com>
2022-09-19 21:59:31 +0530

    Time: 95 ms (89.73%), Space: 131.3 MB (94.52%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.java
@@ -1,15 +1,14 @@
 class Solution {
-    int []ans;
-    int visited[];
+    int[] visited;
     List<Integer> []list;
+    int ans[];
     public int[] countSubTrees(int n, int[][] edges, String labels) {
-        ans=new int[n];
         visited=new int[n];
         list=new LinkedList[n];
-        for(int i=0;i<n;i++)
-        {
-            ans[i]=0;
+        ans=new int[n];
+        for(int i=0;i<n;i++){
             visited[i]=0;
+            ans[i]=0;
             list[i]=new LinkedList<>();
         }
         for(int i=0;i<edges.length;i++)
@@ -17,34 +16,25 @@ class Solution {
             list[edges[i][0]].add(edges[i][1]);
             list[edges[i][1]].add(edges[i][0]);
         }
-        countNodes(0,labels);
+        visit(0,labels);
         return ans;
     }
-    public Map<Character,Integer> countNodes(int indx,String labels)
+    public int[] visit(int indx,String labels)
     {
-        Map<Character,Integer> map=new HashMap<>();
         visited[indx]=1;
-        for(int e:list[indx])
+        int freq[]=new int[26];
+        Arrays.fill(freq,0);
+        freq[labels.charAt(indx)-'a']+=1;
+        for(int ele:list[indx])
         {
-            Map<Character,Integer> tmp=new HashMap<>();
-            if(visited[e]==0) tmp=countNodes(e,labels);
-            Iterator<Map.Entry<Character,Integer>> it=tmp.entrySet().iterator();
-            while(it.hasNext())
+            if(visited[ele]!=1) 
             {
-                Map.Entry<Character,Integer> es=it.next();
-                if(map.containsKey(es.getKey())) map.put(es.getKey(),map.get(es.getKey())+es.getValue());
-                else map.put(es.getKey(),es.getValue());
+                int tmp[]=new int[26];
+                tmp=visit(ele,labels);
+                for(int i=0;i<26;i++) freq[i]+=tmp[i];
             }
         }
-        if(list[indx].size()==0 || (list[indx].size()==1 && indx!=0))
-        {
-            map.put(labels.charAt(indx),1);
-        }
-        else {
-            if(map.containsKey(labels.charAt(indx))) map.put(labels.charAt(indx),map.get(labels.charAt(indx))+1);
-            else map.put(labels.charAt(indx),1);
-        }
-        ans[indx]=map.get(labels.charAt(indx));
-        return map;
+        ans[indx]=freq[labels.charAt(indx)-'a'];
+        return freq;
     }
 }
